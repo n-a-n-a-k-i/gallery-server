05ea6b0765c748135d11c4f40b3f18f7de786b4d
n-a-n-a-k-i <nanaki.god@gmail.com>
2021-09-23 12:12:03 +0300

    update error handler in jwt access token guard

--- src/account/guard/jwt.access.token.guard.ts
+++ src/account/guard/jwt.access.token.guard.ts
@@ -1,8 +1,9 @@
-import {ExecutionContext, Injectable} from "@nestjs/common";
+import {ExecutionContext, Injectable, UnauthorizedException} from "@nestjs/common";
 import {AuthGuard} from "@nestjs/passport";
 import {Reflector} from "@nestjs/core";
 import {Observable} from "rxjs";
 import {IS_PUBLIC_KEY} from "../decorator/public.decorator";
+import {JsonWebTokenError, NotBeforeError, TokenExpiredError} from "jsonwebtoken";
 
 @Injectable()
 export class JwtAccessTokenGuard extends AuthGuard('jwt-access-token') {
@@ -23,6 +24,28 @@ export class JwtAccessTokenGuard extends AuthGuard('jwt-access-token') {
         }
 
         return super.canActivate(context)
+
+    }
+
+    handleRequest(err: any, user: any, info: any, context: any, status: any) {
+
+        if (info instanceof TokenExpiredError) {
+            throw new UnauthorizedException('Срок действия токена доступа истёк')
+        }
+
+        if (info instanceof NotBeforeError) {
+            throw new UnauthorizedException('Текущее время отстаёт от начала действия токена доступа')
+        }
+
+        if (info instanceof JsonWebTokenError) {
+            throw new UnauthorizedException('Недействительная подпись токена доступа')
+        }
+
+        if (info instanceof Error) {
+            throw new UnauthorizedException('Отсутствует токен доступа')
+        }
+
+        return super.handleRequest(err, user, info, context, status);
 
     }
 
